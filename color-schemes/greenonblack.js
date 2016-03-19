8d0dcccab8dfda2d36b8c98e50ceb0198e08c059
Ondrej Cifka <ondra@cifka.com>
2016-03-19 09:24:50 +0100

    Change 'Lime on Black' to better looking 'Green on Black'

---
+++ color-schemes/greenonblack.js
@@ -0,0 +1,38 @@
+colorscheme.addScheme("greenonblack", {
+  "name": "Green on Black",
+  "background": "#0d0d0d",
+  "nodes": {
+    "OFF": {
+      "label": "#202020",
+      "fill": "#121212",
+      "stroke": "#202020"
+    },
+    "GHOST": {
+      "label": "#6db756",
+      "fill": "#448735",
+      "stroke": "#6fb553"
+    },
+    "SUSTAIN": {
+      "label": "#6db756",
+      "fill": "#448735",
+      "stroke": "#6fb553"
+    },
+    "ON": {
+      "label": "#adff8e",
+      "fill": "#43992f",
+      "stroke": "#6bd34a"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#121212",
+      "label-on": "#525252",
+      "fill": "#212121"
+    },
+    "minor": {
+      "label-off": "#121212",
+      "label-on": "#6d6d6d",
+      "fill": "#161616"
+    }
+  }
+});
